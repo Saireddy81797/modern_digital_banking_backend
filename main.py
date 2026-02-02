1fa17da7257ebfe0bfa0d6b8b76bf4f56cd4a39d
Byreddy Sai Reddy <saireddybyreddy72@gmail.com>
2026-02-02 11:51:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ from fastapi import FastAPI
 from routers import accounts
 from routers import transactions
 from routers import budgets
-from routers import auth   # if you have auth router
+from routers import auth
+from routers import bills
+
+# if you have auth router
 
 app = FastAPI(title="Modern Digital Banking Backend")
 
@@ -15,6 +18,8 @@ app.include_router(auth.router)
 app.include_router(accounts.router)
 app.include_router(transactions.router)
 app.include_router(budgets.router)
+app.include_router(bills.router)
+
 
 # =========================
 # ROOT ENDPOINT
